2f982c2619034cf8ce1f67f25ed2e714050d02f4
JDevMen <jdmendoza_90@hotmail.com>
2022-06-30 17:15:48 -0500

    Alerta de salida lista, falta funcionamiento

--- src/components/ProfileComponent/ProfileComponent.tsx
+++ src/components/ProfileComponent/ProfileComponent.tsx
@@ -1,19 +1,39 @@
 import { Button } from "@mui/material";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import AlertaSalida from "../AlertaSalida/AlertaSalida";
 import styles from "./ProfileComponent.module.css";
 
 interface ProfileComponentProps {}
 
-const ProfileComponent: FC<ProfileComponentProps> = () => (
-  <div className={styles.ProfileComponent}>
-    <h4>Puntos: </h4>
-    <Button variant="contained" className={styles.ProfileButton}>
-      Ingresar
-    </Button>
-    <Button variant="contained" className={styles.ProfileButton}>
-      Salir
-    </Button>
-  </div>
-);
+const ProfileComponent: FC<ProfileComponentProps> = () => {
+  const [openSalirDialog, setOpenSalirDialog] = useState(false);
+
+  const handleClickOpenSalirDialog = () => {
+    setOpenSalirDialog(true);
+  };
+  const handleClickCloseSalirDialog = () => {
+    setOpenSalirDialog(false);
+  };
+
+  return (
+    <div className={styles.ProfileComponent}>
+      <h4>Puntos: </h4>
+      <Button variant="contained" className={styles.ProfileButton}>
+        Ingresar
+      </Button>
+      <Button
+        variant="contained"
+        className={styles.ProfileButton}
+        onClick={handleClickOpenSalirDialog}
+      >
+        Salir
+      </Button>
+      <AlertaSalida
+        open={openSalirDialog}
+        handleClose={handleClickCloseSalirDialog}
+      />
+    </div>
+  );
+};
 
 export default ProfileComponent;
