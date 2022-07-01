b028cb0aa2aa5d826e174b28ca8cfd325c07faca
JDevMen <jdmendoza_90@hotmail.com>
2022-06-30 23:15:40 -0500

    Front terminado sin lógica de negocio

--- src/components/ProfileComponent/ProfileComponent.tsx
+++ src/components/ProfileComponent/ProfileComponent.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@mui/material";
 import React, { FC, useState } from "react";
+import AlertaIngreso from "../AlertaIngreso/AlertaIngreso";
 import AlertaSalida from "../AlertaSalida/AlertaSalida";
 import styles from "./ProfileComponent.module.css";
 
 interface ProfileComponentProps {}
 
 const ProfileComponent: FC<ProfileComponentProps> = () => {
+  const [openIngresarDialog, setOpenIngresarDialog] = useState(false);
   const [openSalirDialog, setOpenSalirDialog] = useState(false);
+
+  const handleClickOpenIngresarDialog = () => {
+    setOpenIngresarDialog(true);
+  };
+  const handleClickCloseIngresarDialog = () => {
+    setOpenIngresarDialog(false);
+  };
 
   const handleClickOpenSalirDialog = () => {
     setOpenSalirDialog(true);
@@ -18,7 +27,11 @@ const ProfileComponent: FC<ProfileComponentProps> = () => {
   return (
     <div className={styles.ProfileComponent}>
       <h4>Puntos: </h4>
-      <Button variant="contained" className={styles.ProfileButton}>
+      <Button
+        variant="contained"
+        className={styles.ProfileButton}
+        onClick={handleClickOpenIngresarDialog}
+      >
         Ingresar
       </Button>
       <Button
@@ -28,6 +41,10 @@ const ProfileComponent: FC<ProfileComponentProps> = () => {
       >
         Salir
       </Button>
+      <AlertaIngreso
+        open={openIngresarDialog}
+        handleClose={handleClickCloseIngresarDialog}
+      />
       <AlertaSalida
         open={openSalirDialog}
         handleClose={handleClickCloseSalirDialog}
